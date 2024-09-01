f0879eacf45eaf5e9bd6910d406757b462714925
Leonid Simakov <leonid.simakov@monitel.ru>
2024-09-01 15:16:22 +0300

    rename append; add iterator, pushFront, find, from

--- src/linked-list.ts
+++ src/linked-list.ts
@@ -5,13 +5,36 @@ export class Node<T> {
   ) {}
 }
 
-export class LinkedList<T> {
+export class LinkedList<T> implements Iterable<T> {
   constructor(
     public head: Node<T> | null = null,
     public tail: Node<T> | null = null,
   ) {}
 
-  append(value: T): this {
+  [Symbol.iterator](): IterableIterator<T> {
+    let cursor = this.head;
+
+    return {
+      [Symbol.iterator]() { return this; },
+
+      next(): IteratorResult<T> {
+        if (!cursor) {
+          return { value: null, done: true }
+        }
+
+        const value = cursor.value;
+        cursor = cursor?.next;
+
+        return { value, done: false }
+      }
+    }
+  }
+
+  [Symbol.toPrimitive]() {
+    return JSON.stringify([...this]);
+  }
+
+  pushBack(value: T): this {
     const node = new Node(value);
 
     if (this.head == null || this.tail == null) {
@@ -24,5 +47,37 @@ export class LinkedList<T> {
 
     return this;
   }
+
+  pushFront(value: T): this {
+    const node = new Node(value);
+
+    node.next = this.head;
+    this.head = node;
+
+    if (this.tail == null) {
+      this.tail = this.head;
+    }
+
+    return this;
+  }
+
+  find(comparator: (node: T) => boolean): T | undefined {
+    let cursor = this.head;
+
+    while (cursor && !comparator(cursor.value)) {
+      cursor = cursor.next;
+    }
+  
+    return cursor?.value;
+  }
+
+  static from<T>(iterable: Iterable<T>): LinkedList<T> {
+    const list = new LinkedList<T>();
+    for (const item of iterable) {
+      list.pushBack(item);
+    }
+    return list;
+  }
+
 }
 
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { LinkedList } from "./linked-list";
 
 const list = new LinkedList<number>();
-list.append(1);
-list.append(2);
+list.pushBack(2).pushFront(1).pushBack(3).pushBack(4);
+console.log([...list]);
+console.log(list.find(val => val === 2));
 
-console.log(list);
+console.log(LinkedList.from([10,20,30,40,50])[Symbol.toPrimitive]());
